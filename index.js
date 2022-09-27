4f810326f8f1e9d40129068462ff5c6ed5ce96e8
JuanFe622 <juanf.herrerac@autonoma.edu.co>
2022-09-26 23:27:43 -0500

    Finish the customer model and routes methods

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const newApi = require('./src/routes');
 
 require("dotenv").config();
 const port = 3000 || process.env.PORT;
@@ -9,13 +10,12 @@ app.listen(port, () => {
   console.log("Running in the port ", port);
 });
 
-// mongoose
-//   .connect(process.env.CONNECTION_STRING_MONGODB)
-//   .then(() => {
-//     console.log("Successful connection with MongoDB");
-//   })
-//   .catch(() => {
-//     console.log(err);
-//   });
+mongoose
+  .connect(process.env.CONNECTION_STRING_MONGODB)
+  .then(() => console.log("Successful connection with MongoDB"))
+  .catch((err) => console.log(err));
 
+// Primero accede al index.js de Routes y luego a las rutas de cada modelo
 app.use(express.json());
+newApi(app);
+
